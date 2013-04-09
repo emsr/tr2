f978c546c943abbf50417fd1a979350c69faccad
emsr <3dw4rd@verizon.net>
2013-04-09 17:27:21 -0400

    Add test output. Add test to get to the bottom of the trig integral error!

---
+++ specfun/trigint_error.cpp
@@ -0,0 +1,12 @@
+// /home/ed/bin/bin/g++ -std=c++11 -o trigint_error trigint_error.cpp
+
+#include <iostream>
+#include "cmath"
+
+int
+main()
+{
+  double x = 2.001;
+  double c = cosint(x);
+  double s = sinint(x);
+}
