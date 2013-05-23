ddd670bf74470ee1ff3219bf28e545741b398024
emsr <3dw4rd@verizon.net>
2013-05-23 17:19:23 -0400

    Add optional. I might have something better at home but let's start.

---
+++ optional/test_optional.cpp
@@ -0,0 +1,12 @@
+// /home/ed/bin/bin/g++ -g -std=c++11 -o test_optional test_optional.cpp
+
+// LD_LIBRARY_PATH=/home/ed/bin/lib64:$LD_LIBRARY_PATH ./test_optional
+
+// LD_LIBRARY_PATH=/home/ed/bin/lib64:$LD_LIBRARY_PATH ./test_optional > test_optional.txt 2>&1
+
+#include "optional"
+
+int
+main()
+{
+}
