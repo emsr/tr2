78b02c110e9b6d117787b577085febf43af36699
emsr <3dw4rd@verizon.net>
2013-11-12 09:41:32 -0500

    Fix up string_view with respect to strlen genericity. Add crazy tests to the busybox.

--- test_string_view.cpp
+++ test_string_view.cpp
@@ -46,6 +46,25 @@ main()
   std::cout << "sv.back()     = " << sv.back() << std::endl;
   std::cout << "sv.max_size() = " << sv.max_size() << std::endl;
 
+  unsigned long long ullptr[] = {1ULL, 2ULL, 3ULL, 0ULL};
+  std::experimental::basic_string_view<unsigned long long> ullsv{ullptr};
+  std::cout << std::endl;
+  //std::cout << "ullsv            = " << ullsv << std::endl;
+  std::cout << "ullsv.empty()    = " << std::boolalpha << ullsv.empty() << std::endl;
+  std::cout << "ullsv.size()     = " << ullsv.size() << std::endl;
+  std::cout << "ullsv.length()   = " << ullsv.length() << std::endl;
+  std::cout << "ullsv.max_size() = " << ullsv.max_size() << std::endl;
+
+  using char128_t = unsigned __int128;
+  char128_t u128ptr[] = {1ULL, 2ULL, 3ULL, 4ULL, 5ULL, 0ULL};
+  std::experimental::basic_string_view<char128_t> u128sv{u128ptr};
+  std::cout << std::endl;
+  //std::cout << "u128sv            = " << u128sv << std::endl;
+  std::cout << "u128sv.empty()    = " << std::boolalpha << u128sv.empty() << std::endl;
+  std::cout << "u128sv.size()     = " << u128sv.size() << std::endl;
+  std::cout << "u128sv.length()   = " << u128sv.length() << std::endl;
+  std::cout << "u128sv.max_size() = " << u128sv.max_size() << std::endl;
+
   std::cout << std::endl;
   std::cout << "std::quoted(sv) = " << std::quoted(sv) << std::endl;
 
