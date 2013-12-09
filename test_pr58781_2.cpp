689c28d70580cf8a80c08a37f7d7387ae8927b97
emsr <3dw4rd@verizon.net>
2013-12-09 18:31:57 -0500

    Add shorter test cases to I can actually debug.

---
+++ test_pr58781_2.cpp
@@ -0,0 +1,23 @@
+// /home/ed/bin/bin/g++ -std=c++11 -o test_pr58781_2 test_pr58781_2.cpp
+
+//#include <cstddef>
+
+int
+operator""_s(const char32_t *a, size_t b)
+{
+  return 0;
+}
+
+int
+f()
+{
+  using a = decltype(U"\x1181"_s);
+  using b = decltype(U"\x8111"_s);
+  using c = decltype(U" \x1181"_s);
+}
+
+int
+main()
+{
+  f();
+}
---
+++ test_pr58781_3.cpp
@@ -0,0 +1,15 @@
+// /home/ed/bin/bin/g++ -std=c++11 -o test_pr58781_3 test_pr58781_3.cpp
+
+//#include <cstddef>
+
+int
+operator""_s(const char32_t *a, size_t b)
+{
+  return 0;
+}
+
+int
+f()
+{
+  using a = decltype(U"\x1181"_s);
+}
