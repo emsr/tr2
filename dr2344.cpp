3d1c4264e0f4f19170c6bbc9921dd9b2299c61ec
emsr <3dw4rd@verizon.net>
2014-02-21 13:12:19 -0500

    Add a testcase for DR2344 about quoted and padding.

---
+++ dr2344.cpp
@@ -0,0 +1,30 @@
+//  /home/ed/bin/bin/g++ -std=c++1y -o dr2344 dr2344.cpp
+
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+
+void
+test01()
+{
+  std::ostringstream x, y;
+  x << "[" << std::left << std::setfill('x') << std::setw(20) << R"("AB \"CD\" EF")" << "]";
+  assert(x.str() == "[\"AB \\\"CD\\\" EF\"xxxxxx]");
+  y << "[" << std::left << std::setfill('y') << std::setw(20) << std::quoted(R"(GH "IJ" KL)") << "]";
+  assert(y.str() == "[\"GH \\\"IJ\\\" KL\"yyyyyy]");
+}
+
+#include <iostream>
+
+int
+main()
+{
+  //  Should be: ["AB \"CD\" EF"xxxxxx]
+  //  Gives    : ["AB \"CD\" EF"xxxxxx]
+  std::cout << "[" << std::left << std::setfill('x') << std::setw(20) << R"("AB \"CD\" EF")" << "]" << std::endl;
+  //  Should be: ["GH \"IJ\" KL"yyyyyy]
+  //  Gives    : ["yyyyyyyyyyyyyyyyyyyGH \"IJ\" KL"]
+  std::cout << "[" << std::left << std::setfill('y') << std::setw(20) << std::quoted(R"(GH "IJ" KL)") << "]" << std::endl;
+
+  test01();
+}
