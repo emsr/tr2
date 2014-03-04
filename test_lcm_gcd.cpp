7e214bde97f332f03bda82e41dfd6e6b66df6ca6
Ed Smith-Rowland <3dw4rd@verizon.net>
2014-03-04 11:29:58 -0500

    Add lcm/gcd. Add a net tester.

---
+++ test_lcm_gcd.cpp
@@ -0,0 +1,18 @@
+
+// /home/ed/bin/bin/g++ -std=c++1y -o test_lcm_gcd test_lcm_gcd.cpp
+
+#include <iostream>
+
+#include "numeric"
+
+int
+main()
+{
+  std::cout << "gcd(234,123) = " << gcd(234ULL,123ULL) << '\n';
+  std::cout << "gcd(112,84) = " << gcd(112ULL,84ULL) << '\n';
+  std::cout << "gcd(12457375,1234166) = " << gcd(12457375ULL,1234166ULL) << '\n';
+
+  std::cout << "lcm(234,123) = " << lcm(234ULL,123ULL) << '\n';
+  std::cout << "lcm(112,84) = " << lcm(112ULL,84ULL) << '\n';
+  std::cout << "lcm(12457375,1234166) = " << lcm(12457375ULL,1234166ULL) << '\n';
+}
---
+++ test_net.cpp
@@ -0,0 +1,8 @@
+// /home/ed/bin/bin/g++ -std=c++1y -o test_net test_net.cpp
+
+#include "net"
+
+int
+main()
+{
+}
