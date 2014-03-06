4286e00234937a5777eb55c1dd412a3d6b7d49c8
emsr <3dw4rd@verizon.net>
2014-03-06 15:06:32 -0500

    I think N3644 Null Forward Iterators just works! We might be able to stick a fork into this with a test case.

---
+++ null_iterators.cpp
@@ -0,0 +1,96 @@
+// N3644 Null Forward Iterators
+
+// /home/ed/bin/bin/g++ -std=c++1y -o null_iterators null_iterators.cpp
+
+#include <vector>
+#include <deque>
+#include <cassert>
+/*
+template<template<_Tp> typename _Cont<_Tp>>
+  void
+  test(_Cont<_Tp>)
+  {
+    _Cont<int>::iterator vi1{};
+    _Cont<int>::iterator vi2{};
+    assert(vi1 == vi2);
+
+    _Cont<double>::iterator vd1{};
+    _Cont<double>::iterator vd2{};
+    assert(vd1 == vd2);
+
+    assert(vi1 != vd1);
+  }
+*/
+
+struct A
+{
+  virtual std::vector<int>::const_iterator
+  begin() const
+  { return std::vector<int>::const_iterator{}; }
+
+  virtual std::vector<int>::const_iterator
+  end() const
+  { return std::vector<int>::const_iterator{}; }
+};
+
+A __gA{};
+
+struct B
+: public A
+{
+  virtual std::vector<int>::const_iterator
+  begin() const override
+  { return v.cbegin(); }
+
+  virtual std::vector<int>::const_iterator
+  end() const override
+  { return v.cend(); }
+
+  std::vector<int> v;
+};
+
+B __gB{};
+
+const A&
+get_an_A(int i)
+{
+  if (i % 2)
+    return __gB;
+  else
+    return __gA;
+}
+
+void
+test0()
+{
+  const A& ar = get_an_A(1);
+  const A& as = get_an_A(2);
+
+  for (int x : ar);
+  //  do_something(x);
+  //do_something_else(ar);
+}
+
+int
+main()
+{
+  std::vector<int>::iterator vi1{};
+  std::vector<int>::iterator vi2{};
+  assert(vi1 == vi2);
+
+  std::vector<double>::iterator vd1{};
+  std::vector<double>::iterator vd2{};
+  assert(vd1 == vd2);
+
+  std::deque<int>::iterator di1{};
+  std::deque<int>::iterator di2{};
+  assert(di1 == di2);
+
+  std::deque<double>::iterator dd1{};
+  std::deque<double>::iterator dd2{};
+  assert(dd1 == dd2);
+
+  //assert(vi1 != vd1); // Won't compile.
+
+  //vi1 == vd1; // Won't compile.
+}
