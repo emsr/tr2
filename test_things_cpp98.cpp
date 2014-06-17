10e454fe14bbc2c26e2dc992a238bc7594d2abab
emsr <3dw4rd@verizon.net>
2014-06-17 17:42:12 -0400

    See what is allowed when with test_things_cpp*.cpp -ansi forces c++98?

---
+++ test_things_cpp98.cpp
@@ -0,0 +1,39 @@
+
+// /home/ed/bin/bin/g++ -std=c++98 -o test_things test_things.cpp > /dev/null
+
+// /home/ed/bin/bin/g++ -std=c++98 -pedantic -o test_things test_things.cpp > /dev/null
+
+// /home/ed/bin/bin/g++ -std=c++98 -ansi -o test_things test_things.cpp > /dev/null
+
+auto
+fun(int i)
+-> decltype(i + 5)
+{ return i + 5; }
+
+[[deprecated]]
+int
+bar(int i)
+{ return i + 4; }
+
+int
+main()
+{
+  int bin = 0b010101110;
+  int n = 5;
+  double arr[n];
+
+  int x = 4;
+  auto y = [&r = x, x = x + 1]()
+           -> int
+           {
+             r += 2;
+             return x + 2;
+           }();  // Updates ::x to 6, and initializes y to 7.
+
+  auto glambda = [](auto a, auto&& b) { return  a < b; };
+  bool b = glambda(3, 3.14);
+
+  bar(n);
+
+  int q = 123'456'789;
+}
