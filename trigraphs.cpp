6286f60340b790bce059c7a4e61af582255b47c1
Ed Smith-Rowland <3dw4rd@verizon.net>
2014-06-27 09:42:10 -0400

    Add a trigraphs tester. Nuke that shit from orbit I say!

---
+++ trigraphs.cpp
@@ -0,0 +1,26 @@
+// /home/ed/bin/bin/g++ -std=gnu++98 -trigraphs -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=c++98 -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=gnu++11 -trigraphs -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=c++11 -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=gnu++14 -trigraphs -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=c++14 -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=gnu++17 -o trigraphs trigraphs.cpp
+
+// /home/ed/bin/bin/g++ -std=c++17 -o trigraphs trigraphs.cpp
+
+??=include<complex>
+??=include<iostream>
+
+int
+main()
+??<
+  std::complex<float> arr??(5??);
+??>
+
+// Nuke it from orbit.
