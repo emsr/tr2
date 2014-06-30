d7e0d48c7e9a8106eca337c159520328f1c46be0
emsr <3dw4rd@verizon.net>
2014-06-30 17:26:45 -0400

    Add a teste for PR C++/61648 g++ accepts literal operator friends for non-template classes but not for *template* classes

---
+++ test_pr61648.cpp
@@ -0,0 +1,39 @@
+// /home/ed/bin/bin/g++ -std=c++14 -o test_pr61648 test_pr61648.cpp
+
+#include <cstddef>
+
+struct B {};
+
+B operator+(B, B) { return B(); }
+B operator"" _b(const char *, size_t) { return B(); }
+
+template<char... Str>
+  B operator"" _c() { return B(); }
+
+template<>
+  B operator"" _c<'4','2','.','0'>() { return B(); }
+
+template<typename T>
+  class C
+  {
+    friend B operator+(B, B);
+    friend B operator"" _b(const char *, size_t);
+
+    template<char...>
+      friend B operator"" _c();
+  };
+
+template<>
+  class C<int>
+  {
+    template<char...>
+      friend B operator"" _c();
+  };
+
+template<>
+  class C<float>
+  {
+    template<>friend B operator"" _c<'4','2','.','0'>();
+  };
+
+int main() { return 0; }
