bf4abb291c0b4c6f030f6357953d03eba3add37a
emsr <3dw4rd@verizon.net>
2014-07-01 18:31:55 -0400

    Two lines in typeck.c/check_literal_args and we could have this. For what it's worth... Std issue 1723.

---
+++ test_multichar.cpp
@@ -0,0 +1,16 @@
+
+// /home/ed/bin/bin/g++ -std=c++14 -o test_multichar test_multichar.cpp
+
+int
+operator""_foo(int ch)
+{ return ch; }
+
+int
+operator""_foo(char ch)
+{ return ch; }
+
+int
+main()
+{
+  auto x = 'abcd'_foo;
+}
