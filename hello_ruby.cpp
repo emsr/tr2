4b760d364108b3aeaf1ee6330f0329281a65eb03
emsr <3dw4rd@verizon.net>
2014-07-07 18:56:14 -0400

    Add a thing to test calling methods off a user-defined literal. Works for strings.  Unsupported for numbers.  Think about that though.

---
+++ hello_ruby.cpp
@@ -0,0 +1,13 @@
+// /home/ed/bin/bin/g++ -std=c++14 -o hello_ruby hello_ruby.cpp
+
+#include <string>
+
+int
+main()
+{
+  using namespace std::literals::string_literals;
+  auto len = "Hello, World!!!"s.length();
+}
+
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61057
+// This is about numbers though.
