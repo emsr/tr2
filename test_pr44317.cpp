ab8795db26279a4be80c8d32bcd6f80a8463fe4f
emsr <3dw4rd@verizon.net>
2014-07-14 16:33:47 -0400

    Fixed test_pr44317.cpp.

--- test_pr44317.cpp
+++ test_pr44317.cpp
@@ -1,15 +1,17 @@
-// /home/ed/bin/bin/g++ -std=c++11 -c test_pr44317.cpp
-
-// /home/ed/bin/bin/g++ -std=gnu++11 -c test_pr44317.cpp
-
 class SomeClass {};
-
 #define ASSERT( cnd, ... ) SomeClass(),##__VA_ARGS__
 #define FAIL( ... )        SomeClass(),##__VA_ARGS__
 
-void
-test()
+void test()
 {
   ASSERT( false );
   FAIL(); // { dg-error "expected primary-expression before" }
 }
+
+// /home/ed/bin/bin/g++ -std=c++11 -c test_pr44317.cpp
+
+// /home/ed/bin/bin/g++ -std=c++11 -Wpedantic -c test_pr44317.cpp
+
+// /home/ed/bin/bin/g++ -std=gnu++11 -c test_pr44317.cpp
+
+// /home/ed/bin/bin/g++ -std=gnu++11 -Wpedantic -c test_pr44317.cpp
