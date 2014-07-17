41f7d444d7c0c82e61e13f348cc8fe62caf1ae4f
emsr <3dw4rd@verizon.net>
2014-07-17 17:17:01 -0400

    More confusion.

--- test_pr44317.cpp
+++ test_pr44317.cpp
@@ -1,12 +1,17 @@
 class SomeClass {};
 #define ASSERT( cnd, ... ) SomeClass(),##__VA_ARGS__
 #define FAIL( ... )        SomeClass(),##__VA_ARGS__
+#define TEST(id ...)       SomeClass(),##__VA_ARGS__
 
 void test()
 {
   ASSERT( false );
   FAIL(); // { dg-error "expected primary-expression before" }
 }
+
+// /home/ed/bin/bin/g++ -std=c++98 -c test_pr44317.cpp
+
+// /home/ed/bin/bin/g++ -std=c++98 -Wpedantic -c test_pr44317.cpp
 
 // /home/ed/bin/bin/g++ -std=c++11 -c test_pr44317.cpp
 
