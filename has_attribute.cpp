36f7a41c49674b2a0b2a8139e18a0b08ebf8d9b1
Ed Smith-Rowland <3dw4rd@verizon.net>
2014-08-23 11:16:06 -0400

    Recover from git corruption! Add cool binary IO thing (with a clear screen thing!).

---
+++ has_attribute.cpp
@@ -0,0 +1,13 @@
+
+// /home/ed/bin/bin/g++ -std=c++14 -o has_attribute has_attribute.cpp
+
+#include <cassert>
+
+int
+main()
+{
+  bool ok = __has_attribute(deprecated);
+  assert(ok);
+  bool notok = __has_attribute(urectum);
+  assert(!notok);
+}
