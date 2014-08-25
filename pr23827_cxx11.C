b8a6a19dfd49eab1424b91d402a9cda218e6dba6
emsr <3dw4rd@verizon.net>
2014-08-25 17:38:56 -0400

    Add testscases for PR23827.

--- pr23827_cxx11.C
+++ pr23827_cxx11.C
@@ -1,11 +1,13 @@
-//
+// { dg-do run { target c++11 } }
+// { dg-options "-pedantic-errors" }
 
 #define f (
 #define l )
 #define str(x) #x
 #define xstr(x) str(x)
 
-/* C90 and C++: "0x1p+( 0x1p+)"; C99: "0x1p+f 0x1p+l" */
+// C90 and C++98: "0x1p+( 0x1p+)"
+// C99 and C++11: "0x1p+f 0x1p+l"
 const char *s = xstr(0x1p+f 0x1p+l);
 
 extern "C" void abort (void);
@@ -15,7 +17,7 @@ int
 main()
 {
   if (strcmp (s, "0x1p+( 0x1p+)"))
-    abort ();
+    return 0; // Correct C99 and C++11 behavior.
   else
-    return 0; /* Correct C90 and C++ behavior.  */
+    abort (); // Correct C90 and C++ behavior.
 }
