e1269c136f9707e3d0d2bc5a895a0e1350f0a25d
Ed Smith-Rowland <3dw4rd@verizon.net>
2014-09-04 10:37:12 -0400

    Added .gitignote and one last test for has_attribute.

--- has_attribute.cpp
+++ has_attribute.cpp
@@ -3,13 +3,26 @@
 
 #include <cassert>
 
+#define __has_cpp_attribute(THING) __has_attribute(THING)
+
 int
 main()
 {
   bool ok = __has_attribute(deprecated);
   assert(ok);
+
+  bool gnuok = __has_attribute(gnu::deprecated);
+  assert(gnuok);
+
+  bool oldok = __has_attribute(__deprecated__);
+  assert(oldok);
+
   bool notok = __has_attribute(urectum);
   assert(!notok);
 
+#ifdef __has_cpp_attribute
+# if __has_cpp_attribute(fartknocker)
   bool [[fartknocker]] wtf;
+# endif
+#endif
 }
