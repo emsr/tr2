9757f478ee5e3edeb126e8eb306f74d7e19d58aa
emsr <3dw4rd@verizon.net>
2014-12-17 16:57:23 -0500

    Test sized deallocation - even for C++11.

---
+++ test_sized_dealloc.cpp
@@ -0,0 +1,90 @@
+
+// /home/ed/bin/bin/g++ -g -std=c++14 -o test_sized_dealloc test_sized_dealloc.cpp
+
+// /home/ed/bin/bin/g++ -g -std=c++14 -fno-sized-deallocation -o test_sized_dealloc test_sized_dealloc.cpp
+
+// /home/ed/bin/bin/g++ -g -std=c++11 -o test_sized_dealloc test_sized_dealloc.cpp
+
+// /home/ed/bin/bin/g++ -g -std=c++11 -fsized-deallocation -o test_sized_dealloc test_sized_dealloc.cpp
+
+extern "C" void abort();
+typedef __SIZE_TYPE__ size_t;
+#include <new>
+
+bool called = false;
+
+void
+operator delete[] (void *p, size_t s) throw()
+{
+  called = true;
+  operator delete[] (p);
+}
+
+void
+operator delete (void *p, size_t s) throw()
+{
+  called = true;
+  operator delete (p);
+}
+
+void
+operator delete[] (void *p, size_t s, const std::nothrow_t &) throw()
+{
+  called = true;
+  operator delete[] (p);
+}
+
+void
+operator delete (void *p, size_t s, const std::nothrow_t &) throw()
+{
+  called = true;
+  operator delete (p);
+}
+
+struct A { ~A(){} };
+
+struct B { };
+
+struct C;
+
+struct D { ~D(){}; D() { throw 1; } };
+
+int
+main()
+{
+  //bool check = (__cplusplus >= 201402L);
+  bool check = true;
+
+  delete new int;
+  if (called != check)
+    abort();
+  called = false;
+
+  delete new A;
+  if (called != check)
+    abort();
+  called = false;
+
+  delete[] new A[2];
+  if (called != check)
+    abort();
+  called = false;
+
+  delete new B;
+  if (called != check)
+    abort();
+  called = false;
+
+  /* N3778 added the sized placement deallocation functions, but the core
+     language rules don't provide any way they would be called.  */
+  try { new (std::nothrow) D; } catch (int) {}
+  if (called) abort();
+
+  try { new (std::nothrow) D[2]; } catch (int) {}
+  if (called) abort();
+
+  /* Make sure we don't try to use the size of an array that doesn't have a
+     cookie.  */
+  delete[] new B[2];
+  if (called) abort();
+}
