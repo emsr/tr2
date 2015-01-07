2f57fdee30402a5769dc0c57c678bc16eb4da257
emsr <3dw4rd@verizon.net>
2015-01-07 18:11:07 -0500

    Get the right header and ns for new type traits.

--- test_type_traits.cpp
+++ test_type_traits.cpp
@@ -1,23 +1,23 @@
 
-// /home/ed/bin_var_template_2/bin/g++ -std=c++14 -o test_type_traits test_type_traits.cpp
+// /home/ed/bin/bin/g++ -std=c++14 -o test_type_traits test_type_traits.cpp
 
-#include<type_traits>
+#include<experimental/type_traits>
 
 int
 main()
 {
-  static_assert(std::is_void_v<void> == true, "");
-  static_assert(std::is_void_v<char> == false, "");
+  static_assert(std::experimental::is_void_v<void> == true, "");
+  static_assert(std::experimental::is_void_v<char> == false, "");
 
-  static_assert(std::is_null_pointer_v<std::nullptr_t> == true, "");
-  static_assert(std::is_null_pointer_v<char> == false, "");
+  static_assert(std::experimental::is_null_pointer_v<std::nullptr_t> == true, "");
+  static_assert(std::experimental::is_null_pointer_v<char> == false, "");
 
-  static_assert(std::is_integral_v<int> == true, "");
-  static_assert(std::is_integral_v<float> == false, "");
+  static_assert(std::experimental::is_integral_v<int> == true, "");
+  static_assert(std::experimental::is_integral_v<float> == false, "");
 
-  static_assert(std::is_floating_point_v<float> == true, "");
-  static_assert(std::is_floating_point_v<int> == false, "");
+  static_assert(std::experimental::is_floating_point_v<float> == true, "");
+  static_assert(std::experimental::is_floating_point_v<int> == false, "");
 
-  static_assert(std::is_array_v<char[5]> == true, "");
-  static_assert(std::is_array_v<char> == false, "");
+  static_assert(std::experimental::is_array_v<char[5]> == true, "");
+  static_assert(std::experimental::is_array_v<char> == false, "");
 }
--- test_type_traits_2.cpp
+++ test_type_traits_2.cpp
@@ -1,7 +1,7 @@
 
-// /home/ed/bin_var_template_2/bin/g++ -std=c++14 -o test_type_traits_2 test_type_traits_2.cpp
+// /home/ed/bin/bin/g++ -std=c++14 -o test_type_traits_2 test_type_traits_2.cpp
 
-#include<type_traits>
+#include<experimental/type_traits>
 #include<iostream>
 
 #if __cplusplus >= 201300L // > 201402L
@@ -16,8 +16,8 @@ int
 main()
 {
   std::cout << std::boolalpha;
-  auto c = std::is_void_v<char>;
+  auto c = std::experimental::is_void_v<char>;
   std::cout << "is_void_v<char> = " << c << std::endl;
-  auto v = std::is_void_v<void>;
+  auto v = std::experimental::is_void_v<void>;
   std::cout << "is_void_v<void> = " << v << std::endl;
 }
