2fd23bd2733e3588982a715c93399852f368e8e2
emsr <3dw4rd@verizon.net>
2015-01-30 18:00:16 -0500

    Added concepts toys.

---
+++ concept1-neg.cpp
@@ -0,0 +1,32 @@
+// /home/ed/bin/bin_concepts -std=c++1z -o concept1-neg concept1-neg.cpp
+
+//  Concept specifier.
+//  dcl.spec.concept
+
+//  Function concept.
+template<typename T>
+  concept bool
+  F2(); // error: no definition.
+
+template<typename T>
+  constexpr bool
+  F3();
+
+template<typename T>
+  concept bool
+  F3()
+  { return true; } // error: Redeclaration.
+
+
+//  Variable concept.
+template<typename T>
+  concept bool
+  V1; // error: No initializer.
+
+
+struct S
+{
+  template<typename T>
+    static concept bool
+    C = true; // error: Concept in class scope.
+};
---
+++ concept1.cpp
@@ -0,0 +1,87 @@
+// /home/ed/bin/bin_concepts -std=c++1z -o concept1 concept1.cpp
+
+//  Constrained type specifiers.
+//  dcl.spec.auto.constr
+template<typename T>
+  concept bool C1 = false;
+
+template<int N>
+  concept bool C2 = false;
+
+template<template<typename> typename X>
+  concept bool C3 = false;
+
+template<typename T, int N>
+  class Array
+  {};
+
+template<typename T, template<typename> typename A>
+  class Stack
+  {};
+
+template<typename T>
+  class Alloc
+  {};
+
+void
+f1(C1); // C1 designates a placeholder type.
+
+void
+f2(Array<auto, C2>); // C2 designates a placeholder for int value.
+
+void
+f3(Stack<auto, C3>); // C3 designates a placeholder for a class template.
+
+
+// #1
+template<typename T>
+  concept bool
+  C()
+  { return true; }
+
+// #2
+template<typename T, typename U>
+  concept bool
+  C()
+  { return true; }
+
+// #3
+template<typename T>
+  concept bool
+  D = true;
+
+// The set of concepts referred to by C includes #1 and #2;
+// Concept resolution selects #1.
+void
+f(C);
+
+// The concept-name D refers only to #3.
+void
+g(D);
+
+
+//  Partial-concept-id.
+template<typename T, int N = 0>
+  concept bool
+  Seq = true;
+
+void
+f1(Seq<3>);
+
+void
+f2(Seq<>);
+
+
+//  Concept specifier.
+//  dcl.spec.concept
+
+//  Function concept.
+template<typename T>
+  concept bool
+  F1()
+  { return true; }
+
+//  Variable concept.
+template<typename T>
+  concept bool
+  V1{true};
