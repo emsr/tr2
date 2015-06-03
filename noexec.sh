09491a863ce0a1da9c4518fe0fac4d8959f9e4fd
U-ALIONSCIENCE\ESmith-rowland <ESmith-rowland@SEG_SMTHRWLNDLT.alionscience.com>
2015-06-03 18:40:16 -0400

    Fix permissions.

--- noexec.sh
+++ noexec.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-chmod -R +rw,-x *.tcc *.cpp *.h *.tpp *.cc *.pro bitops variant urectum2 uri UPM-TIREM\ Timeline\ and\ Cost.odt *.c *.in *.out string_literal string_view *.C *.txt patch_* CL_*
+chmod -R +rw,-x *.tcc *.cpp *.h *.tpp *.cc *.pro bitops integer_seq logical_const make_array \
+ net numeric numeric_limits observer_ptr ostream_joiner randint_n4316 random search shared_mutex value_ptr variant \
+ urectum2 uri UPM-TIREM\ Timeline\ and\ Cost.odt *.c *.in *.out string_literal string_view *.C *.txt patch_* CL_* 3.5*
+
+chmod -R +rw,-x ext_distribution/testsuite *.cc
 
 chmod +rwx any binary_io codecvt dynamic_bitset dynarray \
- ext_distribution filesystem ioformat integer_seq logical_const make_array net numeric numeric_limits observer_ptr ostream_joiner \
- randint_n4316 random search shared_mutex value_ptr literals optional ratio SD-6 specfun testinc 3.5*
+ ext_distribution filesystem ioformat \
+ literals optional ratio SD-6 specfun testinc
 
 chmod +rwx .git
 chmod -R +rw .git/*
