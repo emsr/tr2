7822d72cbf6757a30533a5d7d10d2f900b11fa59
Ed Smith-Rowland <ed@pop-os.localdomain>
2022-02-11 15:16:57 -0500

    tidy dijkstra code.

--- comp_geom/dijkstra.cpp
+++ comp_geom/dijkstra.cpp
@@ -8,12 +8,12 @@
 #include <iterator>
 #include <limits>
 
-template<typename _DistTp>
-  _DistTp /* Should be _IdxTp */
-  minDistanceIdx(const std::vector<_DistTp> & dist,
+template<typename DistTp>
+  IdxTp /* Should be IdxTp */
+  minDistanceIdx(const std::vector<DistTp> & dist,
                  const std::vector<bool> & sptSet)
   {
-    _DistTp min = std::numeric_limits<_DistTp>::max();
+    DistTp min = std::numeric_limits<DistTp>::max();
     int min_index;
 
     for (int v = 0; v < dist.size(); ++v)
@@ -29,48 +29,48 @@ template<typename _DistTp>
     return min_index;
   }
 
-template<typename _IdxTp>
+template<typename IdxTp>
   void
-  printPath(const std::vector<_IdxTp> & prev)
+  printPath(const std::vector<IdxTp> & prev)
   {
-    std::vector<_IdxTp> S;
-    _IdxTp u = prev.size() - 1;
-    for (_IdxTp i = 0; i < prev.size() && u != std::numeric_limits<_IdxTp>::max(); ++i)
+    std::vector<IdxTp> S;
+    IdxTp u = prev.size() - 1;
+    for (IdxTp i = 0; i < prev.size() && u != std::numeric_limits<IdxTp>::max(); ++i)
     {
       S.push_back(u);
       u = prev[u];
     }
 
-    std::vector<_IdxTp> v2(S.rbegin(), S.rend());
-    std::copy(v2.begin(), v2.end(), std::ostream_iterator<_IdxTp>(std::cout, ", "));
+    std::vector<IdxTp> v2(S.rbegin(), S.rend());
+    std::copy(v2.begin(), v2.end(), std::ostream_iterator<IdxTp>(std::cout, ", "));
     std::cout << '\n';
   }
 
-template<typename _IdxTp, typename _DistTp, _IdxTp _Num>
+template<typename IdxTp, typename DistTp, IdxTp Num>
   void
-  dijkstra(const _DistTp (&graph)[_Num][_Num], _IdxTp src)
+  dijkstra(const DistTp (&graph)[Num][Num], IdxTp src)
   {
-    std::vector<_IdxTp> prev(_Num);
-    std::vector<_DistTp> dist(_Num, std::numeric_limits<_DistTp>::max());
+    std::vector<IdxTp> prev(Num);
+    std::vector<DistTp> dist(Num, std::numeric_limits<DistTp>::max());
 
-    std::vector<bool> sptSet(_Num, false);
+    std::vector<bool> sptSet(Num, false);
 
     dist[src] = 0;
-    prev[src] = std::numeric_limits<_IdxTp>::max();
+    prev[src] = std::numeric_limits<IdxTp>::max();
 
-    for (int count = 0; count < _Num; ++count)
+    for (int count = 0; count < Num; ++count)
     {
-      _IdxTp u = minDistanceIdx(dist, sptSet);
+      IdxTp u = minDistanceIdx(dist, sptSet);
       //if (u == -1)
 	//break;
 
       sptSet[u] = true;
 
-      for (_IdxTp v = 0; v < _Num; ++v)//_Num - 1
+      for (IdxTp v = 0; v < Num; ++v)//Num - 1
       {
 	if (!sptSet[v]
 	 && graph[u][v]
-	 && dist[u] != std::numeric_limits<_DistTp>::max()
+	 && dist[u] != std::numeric_limits<DistTp>::max()
 	 && dist[u] + graph[u][v] < dist[v])
 	{
           dist[v] = dist[u] + graph[u][v];
